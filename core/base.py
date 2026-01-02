613ba88396306482b10b18c5a69acc60475a19cb
Jane Sinitsyn <sinitsyn.zena@yandex.ru>
2026-01-02 17:08:21 +0400

    Finished BiRNN

--- core/base.py
+++ core/base.py
@@ -1,4 +1,4 @@
-from collections import Counter, defaultdict
+from collections import Counter
 import os
 import re
 import numpy as np
@@ -6,12 +6,6 @@ import pandas as pd
 from nltk import WordPunctTokenizer
 
 import core.vars as vars
-
-def softmax(vector: np.array) -> np.array:
-    return np.exp(vector) / np.exp(vector).sum()
-
-def sigmoid(vector: np.array) -> np.array:
-    return 1 / (1 + np.exp(-vector))
 
 def text_preprocessor(texts: pd.Series, min_freq: int=5) -> tuple[pd.Series, dict[str, int]]:
     tokenizer = WordPunctTokenizer()
@@ -48,78 +42,3 @@ def text_preprocessor(texts: pd.Series, min_freq: int=5) -> tuple[pd.Series, dic
     vocabulary[vars.UNK_VAL] = idx
 
     return tokens, vocabulary
-
-def make_vocabulary(texts_map: dict[str, list[str]], min_appears: int=1, is_save: bool=False, save_dir: str=vars.ASSETS_ROOT, file_name: str="voc.txt"):
-    voc = set()
-    words_count_map = defaultdict(int)
-    for key, text_list in texts_map.items():
-        for word in text_list:
-            words_count_map[word] += 1
-            voc.add(word)
-        # voc |= set(text.split())
-
-    unk_words = set(vars.UNK_VAL)
-
-    for word in voc:
-        if words_count_map[word] <= min_appears:
-            unk_words.add(word)
-
-    voc -= unk_words
-
-    voc = list(voc)
-    voc.append(vars.UNK_VAL)
-
-    vocabulary = dict(zip(voc, range(len(voc))))
-    print(len(vocabulary))
-
-    if is_save:
-        os.makedirs(save_dir, exist_ok=True)
-        with open(os.path.join(save_dir, file_name), "w", encoding="UTF-8") as f:
-            for word in voc:
-                f.write(word + "\n")
-
-    return voc, vocabulary, unk_words
-
-def load_vocabulary(save_dir: str=vars.ASSETS_ROOT, file_name: str="voc.txt"):
-    with open(os.path.join(save_dir, file_name), "r", encoding="UTF-8") as f:
-        voc = f.readlines()
-
-    vocabulary = dict(zip(voc, range(len(voc))))
-    unk_words = []
-                
-    return voc, vocabulary, unk_words
-
-def create_training_batches(words, vocabulary, window_size=2, batch_size=32):
-    """Создание батчей для обучения"""
-    n = len(words)
-    
-    target_batch = []
-    context_batch = []
-    
-    for i in range(n):
-        central_word = words[i]
-        if central_word not in vocabulary:
-            central_word = vars.UNK_VAL
-        
-        # Контекстные слова
-        start = max(0, i - window_size)
-        end = min(n, i + window_size + 1)
-        
-        for j in range(start, end):
-            if j == i:
-                continue
-                
-            context_word = words[j]
-            if context_word not in vocabulary:
-                context_word = vars.UNK_VAL
-            
-            target_batch.append(vocabulary[central_word])
-            context_batch.append(vocabulary[context_word])
-            
-            if len(target_batch) == batch_size:
-                yield np.array(target_batch), np.array(context_batch)
-                target_batch = []
-                context_batch = []
-    
-    if target_batch:
-        yield np.array(target_batch), np.array(context_batch)
--- main.py
+++ main.py
@@ -6,14 +6,16 @@ import core.vars as vars
 import core.base as base
 import os
 
+import matplotlib.pyplot as plt
+
 import gensim.downloader as api
 from gensim.models import KeyedVectors
 
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, roc_auc_score, roc_curve
 from sklearn.model_selection import train_test_split
 
 def naive_baies(texts: pd.Series, targets: pd.Series, delta: float=0.1) -> pd.Series:
-    # Обработка текста
+    # Предобработка текста
     tokens, vocabulary = base.text_preprocessor(texts=texts, min_freq=1)
 
     voc_set = set(vocabulary.keys())
@@ -86,20 +88,11 @@ class VanillaRNN:
         self.Wh = np.random.randn(hidden_size, hidden_size) * 0.01
         self.bh = np.zeros(hidden_size)
 
-        # веса выхода
-        self.Wy = np.random.randn(output_size, 2*hidden_size) * 0.01
-        self.by = np.zeros(output_size)
-
     def tanh(self, x):
         return np.tanh(x)
 
     def tanh_grad(self, h):
         return 1.0 - h ** 2
-
-    def softmax(self, x):
-        x = x - np.max(x)
-        exp = np.exp(x)
-        return exp / np.sum(exp)
     
     def forward(self, xs) -> np.ndarray:
         """
@@ -109,7 +102,7 @@ class VanillaRNN:
 
         self.cache = {
             "xs": [],
-            "hs": [h],   # h0
+            "hs": [h],
         }
 
         for x in xs:
@@ -120,44 +113,14 @@ class VanillaRNN:
             self.cache["hs"].append(h)
 
         return h
-
-    def output_repr(self, h_T):
-        # выход только по последнему состоянию
-        logits = self.Wy @ h_T + self.by
-        probs = self.softmax(logits)
-        self.cache["probs"] = probs
-
-        return logits
-        
-    # def probs(self, logits):
-        
-    #     return probs
-
-    def loss(self, probs, y_true):
-        """
-        y_true: int — индекс правильного класса
-        """
-        return -np.log(probs[y_true] + 1e-9)
     
-    def backward(self, y_true, h_T):
+    def backward(self, dh):
         # инициализация градиентов
         dWx = np.zeros_like(self.Wx)
         dWh = np.zeros_like(self.Wh)
         dbh = np.zeros_like(self.bh)
-        dWy = np.zeros_like(self.Wy)
-        dby = np.zeros_like(self.by)
 
-        # --- градиент выхода ---
-        probs = self.cache["probs"]
-        dlogits = probs.copy()
-        dlogits[y_true] -= 1  # softmax + CE
-
-        dWy += np.outer(dlogits, h_T)
-        dby += dlogits
-
-        dh = self.Wy.T @ dlogits
-
-        # --- BPTT ---
+        # BPTT
         for t in reversed(range(len(self.cache["xs"]))):
             h_t = self.cache["hs"][t + 1]
             h_prev = self.cache["hs"][t]
@@ -171,25 +134,69 @@ class VanillaRNN:
 
             dh = self.Wh.T @ dz
 
-        # gradient clipping (ОЧЕНЬ важно)
-        for grad in [dWx, dWh, dWy]:
-            np.clip(grad, -5, 5, out=grad)
-
         # обновление весов
         self.Wx -= self.alpha * dWx
         self.Wh -= self.alpha * dWh
         self.bh -= self.alpha * dbh
+    
+class BiRNNClassifier:
+    def __init__(self, input_size: int, hidden_size: int, output_size: int, alpha: float=1e-2):
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.alpha = alpha
+        self.cache = {}
+
+        # веса выхода
+        self.Wy = np.random.randn(output_size, 2*hidden_size) * 0.01
+        self.by = np.zeros(output_size)
+
+    def softmax(self, x):
+        x = x - np.max(x)
+        exp = np.exp(x)
+        return exp / np.sum(exp)
+
+    def output_repr(self, h_T):
+        # выход только по последнему состоянию
+        logits = self.Wy @ h_T + self.by
+        probs = self.softmax(logits)
+        self.cache["probs"] = probs
+
+        return logits
+    
+    def probs(self, logits: np.ndarray=None):
+        if logits:
+            return self.softmax(logits)
+        else:
+            return self.cache["probs"]
+
+    def loss(self, probs, y_true):
+        """
+        y_true: int — индекс правильного класса
+        """
+        return -np.log(probs[y_true] + 1e-9)
+    
+    def backward(self, y_true, h_T):
+        # инициализация градиентов
+        dWy = np.zeros_like(self.Wy)
+        dby = np.zeros_like(self.by)
+
+        # градиент выхода
+        probs = self.cache["probs"]
+        dlogits = probs.copy()
+        dlogits[y_true] -= 1  # формула градиента для выхода
+
+        dWy += np.outer(dlogits, h_T)
+        dby += dlogits
+
+        dh = self.Wy.T @ dlogits
+
+        # обновление весов
         self.Wy -= self.alpha * dWy
         self.by -= self.alpha * dby
 
-    # def train_step(self, xs, y_true) -> float:
-    #     h_T = self.forward(xs)
-    #     h_T = self.output_repr(xs)
-    #     h_T = self.probs(xs)
-    #     loss = self.loss(probs, y_true)
-    #     self.backward(y_true)
-    #     return loss
-    
+        return dh[:self.hidden_size], dh[self.hidden_size:]
+
 def check_naive_baies(texts: pd.Series, targets: pd.Series):
     pred_targets = naive_baies(texts=texts, targets=targets)
 
@@ -198,20 +205,8 @@ def check_naive_baies(texts: pd.Series, targets: pd.Series):
     # Сверяем эталон с нашими предсказаниями
     print(accuracy_score(targets, y_pred=pred_targets))
 
-def get_vocabulary(
-        by: str="download", 
-        voc_path: str=os.path.join(vars.ASSETS_ROOT, "vocabulary.pkl")
-    ) -> tuple[typing.Any, int]:
-    if by == "vocabulary":
-        vocabulary = pd.read_pickle(voc_path)
-
-        try:
-            vec_size = vocabulary.iloc[0].shape[0]
-        except Exception as e:
-            vec_size = 0
-
-        return vocabulary, vec_size
-    elif by == "download":
+def get_vocabulary(by: str="download") -> tuple[typing.Any, int]:
+    if by == "download":
         model = api.load('glove-twitter-100', return_path=False)
         return model, model.vector_size
     elif by == "preload":
@@ -229,12 +224,11 @@ def get_vocabulary(
 def get_vec_tokens(
         texts: list[list[str]], 
         vocabulary: pd.Series, 
-        vector_size: int, 
-        voc_path: str
+        vector_size: int
     ):
-    train_tokens = []
+    tokens = []
 
-    print("train_tokens")
+    print("tokens")
 
     UNK = np.zeros(vector_size)
     vocabulary_tokens = []
@@ -250,45 +244,79 @@ def get_vec_tokens(
                 vocabulary_vectors.append(vocabulary[word])
             else:
                 xs.append(UNK)
-        train_tokens.append(xs)
+        tokens.append(xs)
 
-    vocabulary_tokens_series = pd.Series(index=vocabulary_tokens, data=vocabulary_vectors)
+    return tokens
 
-    vocabulary_tokens_series.to_pickle(voc_path)
+def bi_train_step(
+        rnn_f: VanillaRNN,
+        rnn_b: VanillaRNN,
+        rnn_c: BiRNNClassifier,
+        xs: list,
+        y_true_idx: int,
+    ):
+    h_T_forward = rnn_f.forward(xs)
+    h_T_backward = rnn_b.forward(xs[::-1])
+    h_cat = np.concatenate([h_T_forward, h_T_backward])
 
-    return train_tokens
+    logits = rnn_c.output_repr(h_cat)
+    probs = rnn_c.probs()
+    loss = rnn_c.loss(probs, y_true_idx)
 
-def check_vanilla_rnn(texts: pd.Series, targets: pd.Series, voc_path: str=os.path.join(vars.ASSETS_ROOT, "vocabulary.pkl")):
+    dh_f, dh_b = rnn_c.backward(y_true_idx, h_cat)
+    rnn_f.backward(dh_f)
+    rnn_b.backward(dh_b)
+
+    return loss
+
+def show_roc_curve(y_true_all, y_score_all, roc_auc):
+    fpr, tpr, thresholds = roc_curve(y_true_all, y_score_all)
+
+    plt.figure(figsize=(6, 6))
+    plt.plot(fpr, tpr, label=f"BiRNN (AUC = {roc_auc:.3f})")
+    plt.plot([0, 1], [0, 1], linestyle="--", color="gray")
+
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("ROC Curve for BiRNN")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def check_vanilla_rnn(texts: pd.Series, targets: pd.Series):
     print("VanillaRNN")
 
-    vocabulary, vector_size = get_vocabulary(by="preload", voc_path=voc_path)
+    vocabulary, vector_size = get_vocabulary(by="preload")
 
+    # Настраиваем гиперпараметры
     input_size = vector_size
-    hidden_size = 50
+    hidden_size = 20
     output_size = targets.nunique()
-    n_epoches = 25
-    alpha = 0.01
+    n_epoches = 50
+    alpha = 0.003
 
     print("text_preprocessor")
 
+    # Разбиваем выборку на тренировочную и проверочную
     train_texts, test_texts, train_targets, test_targets = train_test_split(texts, targets, test_size=0.2)
 
+    # Предобработка текста
     train_texts_cleared, _ = base.text_preprocessor(texts=train_texts, min_freq=1)
     test_texts_cleared, _ = base.text_preprocessor(texts=test_texts, min_freq=1)
 
+    # Получаем ембеддинги для слов
     train_tokens = get_vec_tokens(
         texts=train_texts_cleared,
         vocabulary=vocabulary,
-        vector_size=vector_size,
-        voc_path=voc_path
+        vector_size=vector_size
     )
     test_tokens = get_vec_tokens(
         texts=test_texts_cleared,
         vocabulary=vocabulary,
-        vector_size=vector_size,
-        voc_path=voc_path
+        vector_size=vector_size
     )
 
+    # Инициализируем 2 RNN - одна для промого обхода, другая для обратного
     rnn_forward = VanillaRNN(
         input_size=input_size,
         hidden_size=hidden_size,
@@ -301,7 +329,15 @@ def check_vanilla_rnn(texts: pd.Series, targets: pd.Series, voc_path: str=os.pat
         output_size=output_size,
         alpha=alpha
     )
+    # Инициализируем классификатор
+    rnn_classifier = BiRNNClassifier(
+        input_size=input_size,
+        hidden_size=hidden_size,
+        output_size=output_size,
+        alpha=alpha
+    )
 
+    # Создаем набор возможных классов (всего 2 класса)
     classes = pd.Series(data=targets.unique())
 
     print("epoches")
@@ -310,58 +346,77 @@ def check_vanilla_rnn(texts: pd.Series, targets: pd.Series, voc_path: str=os.pat
 
     losses = []
 
+    # Начинаем обучение
     for epoch in range(n_epoches):
         epoch_losses = [] 
         for i in range(len(train_texts_cleared)):
             y_true = train_targets.iloc[i]
             y_true_idx = classes[classes == y_true].index
             xs = train_tokens[i]
-            
-            h_T_forward = rnn_forward.forward(xs)
-            h_T_backward = rnn_backward.forward(xs)
-            h_cat = np.concatenate([h_T_forward, h_T_backward])
-            rnn_forward.cache["hs"][-1] = h_cat
-            rnn_backward.cache["hs"][-1] = h_cat
 
-            logits_forward = rnn_forward.output_repr(h_cat)
-            # logits_backward = rnn_forward.output_repr(h_cat)
+            # Делаем шаг обучения
+            loss = bi_train_step(
+                rnn_f=rnn_forward,
+                rnn_b=rnn_backward,
+                rnn_c=rnn_classifier,
+                xs=xs,
+                y_true_idx=y_true_idx
+            )
 
-            loss_forward = rnn_forward.loss(logits_forward, y_true)
+            # Логируем лосс для визуальной проверки в конце - что RNN действительно обучается
+            epoch_losses.append(np.round(loss, 5))
 
-            rnn_forward.backward(y_true, h_cat)
-            rnn_backward.backward(y_true, h_cat)
-
-            epoch_losses.append(np.round(loss_forward, 5))
-
-            # print(f"epoch: {epoch} | idx: {i} | loss: {loss}")
         epoch_losses_series = pd.Series(epoch_losses, name=f"epoch_{epoch+1}")
 
         print(f"epoch: {epoch}")
         losses.append(epoch_losses_series)
 
+    # Смотрим лосс
     print(f"Seconds trained: {time.time() - start_train_time}")
     losses_df = pd.concat(losses, axis=1)
     print(losses_df)
 
+    # Проверяем на тестовой выборке + выводим график ROC-AUC
     y_pred = []
+    y_true_all = []
+    y_score_all = []
 
     print("y_pred")
 
     for i in range(len(test_texts_cleared)):
         xs = test_tokens[i]
 
-        probs = pd.Series(rnn_forward.forward(xs))
+        h_T_forward = rnn_forward.forward(xs)
+        h_T_backward = rnn_backward.forward(xs[::-1])
+        h_cat = np.concatenate([h_T_forward, h_T_backward])
+
+        logits = rnn_classifier.output_repr(h_cat)
+
+        probs = pd.Series(rnn_classifier.probs())
 
         y_true_idx = probs.argmax()
         y_pred.append(classes[y_true_idx])
 
+        y_true_all.append(test_targets.iloc[i])
+        y_score_all.append(probs[1])   # вероятность класса 1
+
     pred_targets = pd.Series(y_pred)
 
+    # Вывод метрики точности
     print(pd.concat([test_targets, pred_targets], axis=1))
-
     print(accuracy_score(test_targets, y_pred=pred_targets))
 
+    # Вывод ROC-AUC
+    roc_auc = roc_auc_score(y_true_all, y_score_all)
+    print(f"ROC-AUC (BiRNN): {roc_auc:.4f}")
+
+    # Визуализация ROC кривой
+    show_roc_curve(y_true_all, y_score_all, roc_auc)
+
 if __name__ == "__main__":
+    os.environ['TCL_LIBRARY'] = r'C:\Users\Warer\AppData\Local\Programs\Python\Python313\tcl\tcl8.6'
+    os.environ['TK_LIBRARY'] = r'C:\Users\Warer\AppData\Local\Programs\Python\Python313\tcl\tk8.6'
+
     data = pd.read_csv(os.path.join(vars.DATASETS_ROOT, "comments.tsv"), sep='\t')
 
     texts = data['comment_text']
